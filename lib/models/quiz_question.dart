9ebf07197861f97619be5389c33d34ad0130dcc4
Hydeoshy Kaled <hydeoshy.kaled65@gmail.com>
2024-07-24 15:11:21 -0300

    project-finalized

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList; //Fazemos um cópia da lista e nesta nossa copia embaralhamos
+  }
+}
